30bb08fc7a2c7c7f1cdac4ba213224a0d4970bd0
uihyeon1229 <101084941+uihyeon1229@users.noreply.github.com>
2022-04-15 15:39:10 +0900

    회원가입

---
+++ src/main/java/com/sparta/hanghae99clone/exception/ExceptionHandler.java
@@ -0,0 +1,36 @@
+package com.sparta.hanghae99clone.exception;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ExceptionHandler {
+
+    @org.springframework.web.bind.annotation.ExceptionHandler(value = { IllegalArgumentException.class })
+    public ResponseEntity<Object> handleApiRequestException(IllegalArgumentException ex) {
+        Exception exception = new Exception();
+        exception.setHttpStatus(HttpStatus.BAD_REQUEST);
+        exception.setErrorMessage(ex.getMessage());
+
+        return new ResponseEntity(
+                exception,
+                HttpStatus.BAD_REQUEST
+        );
+    }
+
+    @org.springframework.web.bind.annotation.ExceptionHandler(value = { NullPointerException.class })
+    public ResponseEntity<Object> handleApiRequestException(NullPointerException ex) {
+        Exception exception = new Exception();
+        exception.setHttpStatus(HttpStatus.BAD_REQUEST);
+        exception.setErrorMessage(ex.getMessage());
+
+        return new ResponseEntity(
+                exception,
+                HttpStatus.BAD_REQUEST
+        );
+    }
+}
+
+
---
+++ src/main/java/com/sparta/hanghae99clone/security/WebSecurityConfig.java
@@ -0,0 +1,77 @@
+package com.sparta.hanghae99clone.security;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sparta.hanghae99clone.repository.UserRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+
+    @Bean
+    public BCryptPasswordEncoder encodePassword() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    public void configure(WebSecurity web) {
+// h2-console 사용에 대한 허용 (CSRF, FrameOptions 무시)
+        web
+                .ignoring()
+                .antMatchers("/h2-console/**");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors();
+        http.csrf().disable();
+
+        // 서버에서 인증은 JWT로 인증하기 때문에 Session의 생성을 막습니다.
+        http
+//                .httpBasic().disable()//rest api 만을 고려하여 기본 설정은 해제하겠습니다.
+//                .exceptionHandling()
+//                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);//토크 기반이라 세션 사용 해제.
+
+        /*
+         * 1.
+         * UsernamePasswordAuthenticationFilter 이전에 FormLoginFilter, JwtFilter 를 등록합니다.
+         * FormLoginFilter : 로그인 인증을 실시합니다.
+         * JwtFilter       : 서버에 접근시 JWT 확인 후 인증을 실시합니다.
+         */
+//        http
+//                .addFilterBefore(formLoginFilter(), UsernamePasswordAuthenticationFilter.class)
+//                .addFilterBefore(jwtFilter(), UsernamePasswordAuthenticationFilter.class);
+
+        http.authorizeRequests()
+                .antMatchers("/user/**").permitAll()
+
+                .anyRequest()
+                .permitAll()
+                .and()
+
+                // [로그아웃 기능]
+                .logout()
+                // 로그아웃 요청 처리 URL
+                .logoutUrl("/user/logout")
+                .permitAll()
+                .and()
+                .exceptionHandling()
+                // "접근 불가" 페이지 URL 설정
+                .accessDeniedPage("/forbidden.html");
+    }
+
+}
