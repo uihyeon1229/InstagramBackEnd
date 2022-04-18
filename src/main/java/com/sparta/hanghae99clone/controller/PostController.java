0c651ac7f2773a0b8413dc92647e3fbc23633089
jaeyoungjang2 <lion85488@gmail.com>
2022-04-18 09:38:08 +0900

    feat: @Authenticationprincipal 어노테이션 추가

--- src/main/java/com/sparta/hanghae99clone/controller/PostController.java
+++ src/main/java/com/sparta/hanghae99clone/controller/PostController.java
@@ -6,13 +6,14 @@ import com.sparta.hanghae99clone.dto.response.PostListResponseDto;
 import com.sparta.hanghae99clone.dto.response.PostResponseDto;
 import com.sparta.hanghae99clone.model.User;
 import com.sparta.hanghae99clone.repository.UserRepository;
+import com.sparta.hanghae99clone.security.UserDetailsImpl;
 import com.sparta.hanghae99clone.service.PostService;
 import java.io.IOException;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -27,15 +28,15 @@ public class PostController {
 
     private final S3Uploader s3Uploader;
     private final PostService postService;
-    private final UserRepository userRepository;
 
     // 단일 게시물 저장
     @PostMapping("/api/posts")
     public String save(
         @RequestParam("images") MultipartFile multipartFile,
-        @RequestParam("contents") String content) throws IOException {
+        @RequestParam("contents") String content,
+        @AuthenticationPrincipal UserDetailsImpl userDetails) throws IOException {
         // 테스트 user
-        User user = userRepository.findById(1L).get();
+        User user = userDetails.getUser();
         // 이미지 업로드
         String uploadImageUrl = s3Uploader.upload(multipartFile);
         // 게시물 저장
@@ -45,9 +46,9 @@ public class PostController {
 
     // 모든 게시물 조회
     @GetMapping("/api/posts")
-    public List<PostListResponseDto> findAll() {
+    public List<PostListResponseDto> findAll(@AuthenticationPrincipal UserDetailsImpl userDetails) {
         // 테스트 user
-        User user = userRepository.findById(1L).get();
+        User user = userDetails.getUser();
         return postService.findAll(user);
     }
 
