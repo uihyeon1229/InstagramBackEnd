ee8ea1ae7a2ae4be73640df33aff5feef3635e15
uchang2001 <uchang2001@hanmail.net>
2022-04-18 21:31:25 +0900

    cors

--- src/main/java/com/sparta/hanghae99clone/security/WebSecurityConfig.java
+++ src/main/java/com/sparta/hanghae99clone/security/WebSecurityConfig.java
@@ -63,6 +63,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
         // 서버에서 인증은 JWT로 인증하기 때문에 Session의 생성을 막습니다.
         http
+                .cors()
+                .and()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
